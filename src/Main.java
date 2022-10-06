6f69cae3ab30072d0808d42a8e251f69874d4f9b
Yamaoki <d89284146236sv@gmail.com>
2022-10-06 17:38:08 +0300

    дз 13

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Author ivanovIvan = new Author("Иван" , "Иванов");
+        Author costenkoDmitri = new Author("Дмитрий" , "Костенко");
+        Book sevenDays = new Book("Семь дней" , ivanovIvan , 2020);
+        Book luckySummer = new Book("Удачное лето" , costenkoDmitri , 2010);
+
+        System.out.println(sevenDays.toString());
+        System.out.println(luckySummer.toString());
+        System.out.println(sevenDays.equals(luckySummer));
+        System.out.println(ivanovIvan.equals(costenkoDmitri));
+        System.out.println(sevenDays.hashCode() + " , " + luckySummer.hashCode());
+        System.out.println(ivanovIvan.hashCode() + " , " + costenkoDmitri.hashCode());
     }
 }
